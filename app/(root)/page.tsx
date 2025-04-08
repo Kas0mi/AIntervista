bcc02e4bea74a65893d1576636f64761c87cd61d
Erion Tufa <eriontufa@outlook.com>
2025-04-08 23:51:05 +0200

    feat: enhance layout and components with navigation, localization, and interview features

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,51 @@
+import InterviewCard from '@/components/InterviewCard';
+import { Button } from '@/components/ui/button';
+import { dummyInterviews } from '@/constants';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const Page = () => {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Pergatit intervisten e punes me Inteligjence Artificiale</h2>
+          <p className="text-lg">
+            Praktikohu me pyetje te verteta dhe merr vleresimin e duhur
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full ">
+            <Link href="/interview">Fillo Intervisten</Link>
+          </Button>
+        </div>
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+      <section className="flex flex-col gap-6 mt-8 ">
+        <h2>Intervistat e tua.</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
+        </div>
+      </section>
+      <section className="flex flex-col gap-6 mt-8 ">
+        <h2>Bej nje interviste</h2>
+        <div className="interviews-section">
+          {dummyInterviews.map((interview) => (
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
+
+          {/* <p>Nuk ke bere ende asnje interviste</p> */}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Page;
