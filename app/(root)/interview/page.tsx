1ae88bcbccb073841157e8e1abd1206f5ba80272
Erion Tufa <eriontufa@outlook.com>
2025-04-11 16:37:01 +0200

    feat: add Agent component with call status and user interaction features

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,13 @@
+import Agent from '@/components/Agent';
+import React from 'react';
+
+const Page = () => {
+  return (
+    <>
+      <h3>Krijimi i Intervistes</h3>
+      <Agent username="You" userId="user1" type="generate" />
+    </>
+  );
+};
+
+export default Page;
