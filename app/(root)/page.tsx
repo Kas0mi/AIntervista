83e5637cbbaaf242f7a6caf0354d5fcc02a7ad41
Erion Tufa <eriontufa@outlook.com>
2025-04-12 16:55:10 +0200

    feat: implement user interview retrieval functions and update Page component to display user-specific interviews

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,25 @@
 import InterviewCard from '@/components/InterviewCard';
 import { Button } from '@/components/ui/button';
 import { dummyInterviews } from '@/constants';
+import {
+  getCurrentUser,
+  getInterviewsByUserId,
+  getLatestInterviews,
+} from '@/lib/actions/auth.action';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
-const Page = () => {
+const Page = async () => {
+  const user = await getCurrentUser();
+  const [userInterviews, latestInterviews] = await Promise.all([
+    await getInterviewsByUserId(user?.id!),
+    await getLatestInterviews({ userId: user?.id! }),
+  ]);
+
+  const hasPastInterviews = userInterviews?.length > 0;
+  const hasUpcommingInterviews = latestInterviews?.length > 0;
+
   return (
     <>
       <section className="card-cta">
@@ -29,19 +43,25 @@ const Page = () => {
       <section className="flex flex-col gap-6 mt-8 ">
         <h2>Intervistat e tua.</h2>
         <div className="interviews-section">
-          {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id} />
-          ))}
+          {hasPastInterviews ? (
+            userInterviews?.map((interview) => (
+              <InterviewCard {...interview} key={interview.id} />
+            ))
+          ) : (
+            <p>Nuk ke bere ende asnje interviste</p>
+          )}
         </div>
       </section>
       <section className="flex flex-col gap-6 mt-8 ">
         <h2>Bej nje interviste</h2>
         <div className="interviews-section">
-          {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id} />
-          ))}
-
-          {/* <p>Nuk ke bere ende asnje interviste</p> */}
+          {hasUpcommingInterviews ? (
+            latestInterviews?.map((interview) => (
+              <InterviewCard {...interview} key={interview.id} />
+            ))
+          ) : (
+            <p>Nuk ka intervista te vlefshme per momentin.</p>
+          )}
         </div>
       </section>
     </>
